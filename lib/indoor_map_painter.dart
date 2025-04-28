bb74d19487597920124afe7fc77d126c53cf9eee
naveenmadur <naveen.madur@coditas.com>
2025-04-28 14:39:30 +0530

    Separated Indoor map painter

---
+++ lib/indoor_map_painter.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:geojson_vi/geojson_vi.dart';
+
+class IndoorMapPainter extends CustomPainter {
+  final GeoJSONFeatureCollection featureCollection;
+  late final double mapWidth;
+  late final double mapHeight;
+
+  IndoorMapPainter(this.featureCollection) {
+    _calculateMapSize();
+  }
+
+  void _calculateMapSize() {
+    double maxX = 0;
+    double maxY = 0;
+
+    for (GeoJSONFeature? feature in featureCollection.features) {
+      if (feature?.geometry is GeoJSONPolygon) {
+        final polygon = feature?.geometry as GeoJSONPolygon;
+        for (var ring in polygon.coordinates) {
+          for (var point in ring) {
+            if (point[0] > maxX) maxX = point[0];
+            if (point[1] > maxY) maxY = point[1];
+          }
+        }
+      }
+    }
+
+    mapWidth = maxX;
+    mapHeight = maxY;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final scaleX = size.width / mapWidth;
+    final scaleY = size.height / mapHeight;
+    final paint = Paint()
+      ..color = Colors.blueAccent.withValues(alpha: 0.5)
+      ..style = PaintingStyle.fill;
+
+    final borderPaint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke;
+
+    for (var feature in featureCollection.features) {
+      if (feature?.geometry is GeoJSONPolygon) {
+        final polygon = feature?.geometry as GeoJSONPolygon;
+
+        final path = Path();
+        for (var ring in polygon.coordinates) {
+          if (ring.isEmpty) continue;
+
+          path.moveTo(
+            ring[0][0] * scaleX,
+            ring[0][1] * scaleY,
+          );
+
+          for (var point in ring.skip(1)) {
+            path.lineTo(
+              point[0] * scaleX,
+              point[1] * scaleY,
+            );
+          }
+          path.close();
+        }
+
+        canvas.drawPath(path, paint);
+        canvas.drawPath(path, borderPaint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
